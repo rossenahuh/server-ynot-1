a07a425c6b179fc86dcb4730b2a9a232fab5e3c0
rossenahuh <rossenahuh@gmail.com>
2019-04-03 22:54:58 +0900

    modified averageRating API

--- routes/reviews.js
+++ routes/reviews.js
@@ -3,6 +3,5 @@ const router = express.Router();
 const reviewsController = require('../controllers/reviews');
 
 router.get('/reviewoftheday', reviewsController.getReviewOfTheDay);
-router.get('/averageRating', reviewsController.getAverageRatingOfTheRestaurant);
 router.post('/', reviewsController.insertReview);
 module.exports = router;
